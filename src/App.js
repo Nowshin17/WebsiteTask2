0d94ce99152dda33f59c57e04a5a5c211e72b44d
nowshin2 <nowshin@algosolver.com>
2022-06-01 21:45:47 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Footer from './Component/Footer/Footer';
+import Navbar from './Component/Navbar/Navbar';
+import Section1 from './Component/Sections/Section1';
+import Slider from './Component/Sections/Slider';
+import Section10 from './Component/Sections/Section10';
+import Section11 from './Component/Sections/Section11';
+
+
+
+
+
+function App() {
+  return (
+    <div>
+    
+     <Navbar />
+     <Slider />
+     <Section1 />
+     <Section10 />
+     <Section11 />
+     <Footer />
+     
+
+     
+    
+      
+ </div>
+  );
+}
+
+export default App;
