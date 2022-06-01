0069f7cb4d2a9ac88d9342b39adba8bca50659a0
nowshin2 <nowshin@algosolver.com>
2022-06-01 21:07:11 +0600

    Create Section10.js

---
+++ src/Component/Sections/Section10.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import '../Styles/style_section.scss';
+
+
+
+
+function Section10() {
+  return (
+<div>
+<div className='background-color4 alignItem p'>
+        <div className='text-center'>
+            <div class="text-4xl font-bold pt-16 text-[#040D34]"> Latest Blog & News</div> 
+        </div>
+    <div className='flex gap-6 pt-10'>
+
+        <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <img class="rounded-t-lg" src="./card3.png" alt="" />
+            </a> 
+
+
+            <div class="p-5">
+            <div class="text-[#ff6752] text-1xl font-semibold pt-2 ">EDUCATION- JAN 21, 2022</div>
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight hover:underline text-gray-900 dark:text-white">Educational Technology & Mobile Learning</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Dramatically supply tranparent deliverables before backward in reverse chronological order.</p>   
+            </div>
+        </div>
+
+        <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <img class="rounded-t-lg" src="./card3.png" alt="" />
+            </a> 
+
+
+            <div class="p-5">
+            <div class="text-[#ff6752] text-1xl font-semibold pt-2 ">EDUCATION- JAN 21, 2022</div>
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight hover:underline text-gray-900 dark:text-white">Educational Technology & Mobile Learning</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Dramatically supply tranparent deliverables before backward in reverse chronological order.</p>   
+            </div>
+        </div>
+
+        <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <img class="rounded-t-lg" src="./card3.png" alt="" />
+            </a> 
+            <div class="p-5">
+            <div class="text-[#ff6752] text-1xl font-semibold pt-2">EDUCATION- JAN 21, 2022</div>
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight hover:underline text-gray-900 dark:text-white">Educational Technology & Mobile Learning</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Dramatically supply tranparent deliverables before backward in reverse chronological order.</p>   
+            </div>
+        </div>
+
+        <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <img class="rounded-t-lg" src="./card3.png" alt="" />
+            </a> 
+            <div class="p-5">
+            <div class="text-[#ff6752] text-1xl font-semibold pt-2">EDUCATION- JAN 21, 2022</div>
+                <a href="#">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight hover:underline text-gray-900 dark:text-white">Educational Technology & Mobile Learning</h5>
+                </a>
+                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Dramatically supply tranparent deliverables before backward in reverse chronological order.</p>   
+            </div>
+        </div>
+
+    </div>
+
+    <div className='logo pt-10 text-center'>
+        <button class="bg-[#ff6752] text-white font-medium py-2 px-4 border border-[#ff6752] rounded">
+            Learn More
+        </button>
+    </div>
+
+</div>
+</div>
+  )
+}
+
+export default Section10
