7eff8d029dc78d26a7ebff7e7bb385bb6da9993a
nowshin2 <nowshin@algosolver.com>
2022-06-01 21:07:29 +0600

    Create Section11.js

---
+++ src/Component/Sections/Section11.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import '../Styles/style_section.scss';
+
+function Section11() {
+  return (
+    <div>
+        <div className='background-color11 alignItem '>
+            <div className='text-center'>
+                <div class="text-4xl font-bold pt-1">Our Key Supports</div> 
+                <div class="flex gap-40 pt-16">
+                    <div class="box-border h-30 w-40 p-4 shadow-2xl"> 
+                        <div class="pt-4 pl-1">
+                        <img src="./1t.png" alt="img" /> 
+                        
+                        </div>
+                    </div>
+
+                    <div class="box-border h-30 w-40 p-4 shadow-2xl"> 
+                        <div class="pt-4 pl-1">
+                            <img src="./loho2.png" alt="img" /> 
+                        </div>
+                    </div>
+
+                    <div class="box-border h-30 w-40 p-4 shadow-2xl"> 
+                    <div class="pt-4 pl-1">
+                        <img src="./loh3.png" alt="img" /> 
+                        
+                        </div>
+                    </div>
+
+                    <div class="box-border h-30 w-40 p-4 shadow-2xl"> 
+                    <div class="pt-4 pl-1">
+                        <img src="./logo4.png" alt="img" /> 
+                        
+                        </div>
+                    </div>
+
+                    <div class="box-border h-30 w-40 p-4 shadow-2xl"> 
+                    <div class="pt-4 pl-1">
+                        <img src="./logo5.png" alt="img" /> 
+                        
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Section11
